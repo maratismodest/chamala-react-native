54c8b209a8a2a79c11dc691608f6f8874cbcdb4d
Marat Faizerakhmanov <maratismodest@gmail.com>
2023-12-25 01:23:25 +0300

    finally

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -2,12 +2,14 @@ import Happy from "@assets/svg/happy.svg";
 import Button from "@components/Button";
 import {Text, View} from "@components/Themed";
 import {LocaleContext} from "@providers/LocaleProvider";
-import {useIsFocused} from "@react-navigation/native";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 import {deleteAsyncData, getAsyncData, storeAsyncData,} from "@store/async-storage";
 import {appStyles} from "@styles";
 import * as Google from "expo-auth-session/providers/google";
 import * as WebBrowser from "expo-web-browser";
 import React, {useContext, useEffect, useState} from "react";
+
+import {Image} from 'react-native'
 import {env} from "../../data/env";
 
 interface Profile {
@@ -28,39 +30,40 @@ const authRequestConfig = {
   webClientId: env.WEB_CLIENT_ID,
 };
 
+const ShowUserInfo = ({userInfo}: any) => {
+  return (
+    <View style={{alignItems: 'center', justifyContent: 'center'}}>
+      <Image source={{uri: userInfo.picture}} style={{width: 100, height: 100, borderRadius: 50}}/>
+      <Text style={{fontSize: 20, fontWeight: 'bold'}}>{userInfo.name}</Text>
+    </View>
+  )
+}
+
 export default function ProfilePage() {
   const {setLocale, i18n} = useContext(LocaleContext)
-
-  const isFocused = useIsFocused();
-  const [profile, setProfile] = useState<Profile | undefined>();
-  useEffect(() => {
-    console.log("called");
-
-    // Call only when screen open or when back on screen
-    if (isFocused) {
-      getInitialData();
-    }
-  }, [isFocused]);
-
-
-  const [accessToken, setAccessToken] = React.useState<string | undefined>();
-  const [userInfo, setUserInfo] = React.useState<any>();
-  const [message, setMessage] = React.useState();
+  const [userInfo, setUserInfo] = useState<any>(null);
   const [request, response, promptAsync] =
     Google.useAuthRequest(authRequestConfig);
 
-  React.useEffect(() => {
-    // @ts-ignore
-    setMessage(JSON.stringify(response));
-    if (response?.type === "success") {
-      // @ts-ignore
-      setAccessToken(response.authentication.accessToken);
-    } else {
-      setAccessToken(undefined);
-    }
-  }, [response]);
+  const [profile, setProfile] = useState<any>()
 
-  const getInitialData = async () => {
+  const getUserInfo = async (token: any) => {
+    if (!token) return
+    try {
+      const response = await fetch("https://www.googleapis.com/userinfo/v2/me", {
+        headers: {Authorization: `Bearer ${token}`}
+      });
+      const user = await response.json();
+      await AsyncStorage.setItem('@user', JSON.stringify(user))
+      setUserInfo(user);
+    } catch (e) {
+      console.error('e', e)
+    }
+
+  }
+
+
+  const getInitialData = () => {
     getAsyncData("statistics").then((res) => {
       if (!res) {
         storeAsyncData("statistics", initialProfile).then(() =>
@@ -69,13 +72,28 @@ export default function ProfilePage() {
       } else {
         setProfile(JSON.parse(res));
       }
-    });
-  };
+    })
+  }
 
-  const signOut = () => {
-    setAccessToken(undefined);
-    setUserInfo(undefined);
-  };
+  useEffect(() => {
+    getInitialData()
+  }, []);
+
+  useEffect(() => {
+    signIn()
+  }, [response]);
+
+  const signIn = async () => {
+    const user = await AsyncStorage.getItem('@user')
+    if (!user) {
+      if (response?.type === 'success') {
+        await getUserInfo(response?.authentication?.accessToken)
+      }
+
+    } else {
+      setUserInfo(JSON.parse(user))
+    }
+  }
 
   return (
     <View style={appStyles.container}>
@@ -83,6 +101,7 @@ export default function ProfilePage() {
         <Happy width={96} height={96}/>
       </View>
       <Text style={appStyles.h1}>{i18n.t("profile")}</Text>
+      {userInfo && <ShowUserInfo userInfo={userInfo}/>}
       {profile && (
         <View>
           <Text style={appStyles.text}>
@@ -116,26 +135,29 @@ export default function ProfilePage() {
           />
         </View>
       )}
-      <View>
 
-        <View style={{flexDirection: 'row', justifyContent: 'space-between', gap: 16}}>
-          <Button title='Ru' onPress={() => setLocale('ru')}/>
-          <Button title='En' onPress={() => setLocale('en')}/>
-        </View>
-        <Text>...</Text>
-        <Button
-          title={accessToken ? i18n.t("login") : i18n.t("logout")}
-          onPress={
-            accessToken
-              ? signOut
-              : () => {
-                // @ts-ignore
-                promptAsync({useProxy: false, showInRecents: true});
-              }
-          }
-        />
+      <View style={{flexDirection: 'row', justifyContent: 'space-between', gap: 16}}>
+        <Button title='Ru' onPress={() => setLocale('ru')}/>
+        <Button title='En' onPress={() => setLocale('en')}/>
       </View>
-
+      <View style={{marginTop: 16}}>
+        {!userInfo && <Button
+            title={i18n.t("login")}
+            disabled={!request}
+            onPress={
+              () => promptAsync()
+            }
+        />}
+        {userInfo && <Button
+            title={i18n.t("logout")}
+            onPress={
+              async () => {
+                await AsyncStorage.removeItem('@user')
+                setUserInfo(undefined)
+              }
+            }
+        />}
+      </View>
     </View>
   );
 }
