83d0cf7206ca8e176379cf7a44fda933a6ec4ebd
Marat Faizerakhmanov <maratismodest@gmail.com>
2024-08-11 12:30:25 +0300

    update

--- data/env.ts
+++ data/env.ts
@@ -1,4 +1,7 @@
-const ANDROID_CLIENT_ID = '608106149141-oqh4a1bvonl1nl49k4gt8tse13vvi6u8.apps.googleusercontent.com'
-const IOS_CLIENT_ID = '608106149141-fqees75kvitmk53gv762pdd4hnsljf6h.apps.googleusercontent.com'
-const WEB_CLIENT_ID = '608106149141-kaadhb6t6mm7r5sj2b91d9gten5mrm40.apps.googleusercontent.com'
-export const env = {ANDROID_CLIENT_ID, IOS_CLIENT_ID, WEB_CLIENT_ID}
+const ANDROID_CLIENT_ID =
+  "608106149141-oqh4a1bvonl1nl49k4gt8tse13vvi6u8.apps.googleusercontent.com";
+const IOS_CLIENT_ID =
+  "608106149141-fqees75kvitmk53gv762pdd4hnsljf6h.apps.googleusercontent.com";
+const WEB_CLIENT_ID =
+  "608106149141-kaadhb6t6mm7r5sj2b91d9gten5mrm40.apps.googleusercontent.com";
+export const env = { ANDROID_CLIENT_ID, IOS_CLIENT_ID, WEB_CLIENT_ID };
